65d45471b937579ae5b5fdf9e9e6c86c5dd14a81
skloeckner <seankloeckner@gmail.com>
2023-05-13 16:04:29 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,45 @@
 import discord
 import random
-import os
 
-from utils import load_phrases
+# Define the list of curse words to check against
+CURSE_WORDS = ['fuck', 'shit', 'bitch', 'asshole', 'dick', 'pussy', 'cock', 'cunt', 'motherfucker', 'twat', 'bastard', 'damn', 'hell', 'fag', 'nigga', 'slut', 'whore']
 
+# Define the list of scolding phrases
+PHRASES = [
+    "Now now, watch your language young one!",
+    "In my day, we didn't use language like that!",
+    "Such foul language, young people these days...",
+    "There's no need for that kind of language!",
+    "Please refrain from using that kind of language in this server!",
+    "I'm going to have to wash your mouth out with soap if you keep that up!",
+    "That kind of language is not acceptable!",
+    "This is a family-friendly server, please keep your language appropriate!",
+    "Language like that will not be tolerated here!",
+    "Do you kiss your mother with that mouth?",
+    "That kind of language is a no-no!",
+    "Watch your mouth, young one!",
+    "No cursing allowed in this server!"
+]
+
+# Define the Discord client
 client = discord.Client()
 
-phrases = load_phrases(os.path.join(os.getcwd(), 'data', 'phrases.txt'))
-
+# Define the on_ready event handler
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+# Define the on_message event handler
 @client.event
 async def on_message(message):
+    # Ignore messages from the bot itself to avoid infinite loops
     if message.author == client.user:
         return
 
-    # List of curse words to check against
-    curse_words = ['heck', 'darn', 'blast', 'gosh']
+    # Check if the message contains a curse word
+    if any(word in message.content.lower() for word in CURSE_WORDS):
+        # Choose a random scolding phrase and send it as a message
+        await message.channel.send(random.choice(PHRASES))
 
-    # Check if message contains a curse word
-    for word in curse_words:
-        if word in message.content.lower():
-            # Send scolding message with a random phrase
-            phrase = random.choice(phrases)
-            await message.channel.send(f"Oh my goodness, {message.author.mention}! {phrase} Please watch your language and refrain from using such naughty words like '{word}'!")
-
-client.run('YOUR_DISCORD_BOT_TOKEN')
+# Run the bot using the provided API key
+client.run('YOUR_DISCORD_API_KEY_HERE')
